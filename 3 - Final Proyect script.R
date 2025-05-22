1109c69fe2c44dba45b60619aa825ff85b68c193
LiamMontero <liammonteroguillemi@gmail.com>
2025-05-22 04:11:53 +0400

    I add the data exploratory

--- 3 - Final Proyect script.R
+++ 3 - Final Proyect script.R
@@ -69,9 +69,129 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 # Data Exploratory
 ###################
 
-head(edx)
+if(!require(tibble)) install.packages("tibble")
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(lubridate)) install.packages("lubridate")
+if(!require(ggthemes)) install.packages("ggthemes")
+if(!require(scales)) install.packages("scales")
+if(!require(knitr)) install.packages("knitr")
+if(!require(kableExtra)) install.packages("kableExtra")
+if(!require(dslabs)) install.packages("dslabs")
+if(!require(gam)) install.packages("gam")
+if(!require(gridExtra)) install.packages("gridExtra")
+
+library(tibble)
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(ggthemes)
+library(scales)
+library(knitr)
+library(kableExtra)
 
 
+# Set a seed for reproducibility
+set.seed(1)
+
+# Check the number of rows in the reduced dataset
+data.frame(Rows = nrow(edx))
+
+
+# Show head of dataset edx (first 10 rows)
+head(edx, 10)
+
+# Plot distribution of film ratings in edx
+ggplot(edx, aes(x = rating)) +
+  geom_histogram(binwidth = 0.5, fill = "skyblue", color = "black") +
+  scale_x_continuous(breaks = seq(min(edx$rating)-0.5, max(edx$rating), by = 0.5)) +
+  scale_y_continuous(breaks = seq(0, max(length(edx$rating)), by = 500000),labels = function(x) format(x, scientific = FALSE,big.mark=",")) +
+  labs(x = "Rating", y = "Number of ratings", caption = "Source: edx data")
+
+
+library(dplyr)
+
+# Plot Top 10 Films with the most ratings in a table
+edx %>%
+  group_by(movieId, title) %>%
+  summarise(count = n(), .groups = 'drop') %>%
+  arrange(desc(count)) %>%
+  head(10)
+
+
+# Convert timestamp to Date format for plotting
+edx$date <- as.Date(as.POSIXct(edx$timestamp, origin = "1970-01-01"))
+
+# Create a line plot showing the number of ratings per week
+ggplot(edx, aes(x = floor_date(as.Date(as.POSIXct(timestamp, origin = "1970-01-01")), "week"))) +
+  geom_line(stat = "count", color = "darkgreen") +
+  labs(x = "Year", 
+       y = "Number of ratings", 
+       caption = "Source: edx data") +
+  scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  scale_y_continuous(trans = "sqrt", labels = function(x) format(x, scientific = FALSE,big.mark=","))
+
+
+# user_prep: Preparing data by extracting release year from movie titles
+edx$released <- str_extract(edx$title, "\\((\\d{4})\\)") %>% 
+  str_remove_all("[()]") %>% 
+  as.numeric()
+
+
+# user_plots: Creating two side-by-side histograms for average rating per user and number of ratings per user
+library(gridExtra)
+
+grid.arrange(
+  
+  # Plot 1: Histogram of average rating per user
+  edx %>% group_by(userId) %>%
+    summarise(ave_rating = sum(rating)/n()) %>%
+    ggplot(aes(ave_rating)) +
+    geom_histogram(bins=30, fill = "skyblue", color = "black") +
+    scale_y_continuous(labels = function(x) format(x, scientific = FALSE,big.mark=",")) +
+    labs(x = "Average rating", y = "Number of users", caption = "Source: edx data"),
+  
+  # Plot 2: Histogram of number of ratings per user
+  edx %>% 
+    count(userId) %>% 
+    ggplot(aes(n)) + 
+    geom_histogram(bins=30, fill = "skyblue", color = "black") +
+    scale_x_log10(labels = function(x) format(x, scientific = FALSE,big.mark=",")) +
+    scale_y_continuous(labels = function(x) format(x, scientific = FALSE,big.mark=",")) +
+    labs(x = "Users", y = "Number of ratings", caption = "Source: edx data"),
+  
+  # Arrange plots in 2 columns and set width  
+  ncol = 2, 
+  widths = c(1, 1)
+)
+
+
+# time_dependent_plots: Plot weekly averaged rating vs. date and averaged rating vs. release year
+library(gridExtra)
+
+grid.arrange(
+  # Scatter plot of weekly averaged rating over time
+  edx %>% 
+    ggplot(aes(date, rating)) +
+    geom_point(data = edx %>% 
+                 mutate(date = round_date(date, unit = "week")) %>%
+                 group_by(date) %>%
+                 summarize(rating = mean(rating)),
+               aes(x = date, y = rating), 
+               alpha = 0.5, color = "black", size = 0.7) +
+    labs(x = "Date", y = "Rating", caption = "Source: edx data") +
+    coord_cartesian(ylim = c(3, 4)),
+  
+  # Scatter plot of averaged rating by release year
+  edx %>%
+    group_by(released) %>%
+    summarize(rating = mean(rating)) %>%
+    ggplot(aes(released, rating)) +
+    geom_point(size = 0.7) +
+    labs(x = "Released", y = "Rating", caption = "Source: edx data"),
+  
+  ncol = 2, 
+  widths = c(1, 1)
+)
 
 
 
